6c2ae20e975f5d6a33ea299258a3bd8d2e57c796
komstin <komstin@mail.ru>
2017-10-18 13:23:16 +0300

    Add files via upload

---
+++ monyze-agent.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+''' Hello from monyze-agent.py '''
+
+__author__ = 'Konstantin Lyakhov'
+__contact__ = 'kl@rifco.ru'
+__copyright__ = 'Copyright (C) Monyze. All rights reserved.'
+__credits__ = ['Konstantin Lyakhov', 'Dmitry Soloviev']
+__license__ = ''
+
+import hashlib  # necessary for compilation
+import json  # necessary for compilation
+from monyze import parse
+
+
+def main():
+    ''' Monyze monitoring agent '''
+
+    parse()
+
+
+if __name__ == '__main__':
+
+    main()
+
+# TODO: Logging

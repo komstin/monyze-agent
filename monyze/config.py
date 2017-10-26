6e3aecf8318ad0ac79e4966dac465a7f188d78ab
komstin <komstin@mail.ru>
2017-10-26 12:45:13 +0300

    Update config.py

--- monyze/config.py
+++ monyze/config.py
@@ -4,7 +4,7 @@
 __author__ = 'Konstantin Lyakhov'
 __contact__ = 'kl@rifco.ru'
 __copyright__ = 'Copyright (C) Monyze. All rights reserved.'
-__credits__ = ['Konstantin Lyakhov', 'Dmitry Soloviev']
+__credits__ = ['Konstantin Lyakhov (Skype: komstin)', 'Dmitry Soloviev (Skype: gex_skype)']
 __license__ = ''
 
 import datetime
